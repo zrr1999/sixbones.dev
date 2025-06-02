8b8ad35c32e29f630a59aa92fce75b65369cd6cc
zrr1999 <2742392377@qq.com>
2025-06-02 12:34:49 +0800

    ♻️ refactor: rename content to data

--- apps/blog/src/content.config.ts
+++ apps/blog/src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 import { SITE } from "@/config";
 
-export const BLOG_PATH = "src/content/blog";
+export const BLOG_PATH = "src/data/blog";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: `./${BLOG_PATH}` }),
